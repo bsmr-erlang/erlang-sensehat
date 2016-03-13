7b7cb4c1d0c16746f7c8afff6082d79c24e666e4
Morten Teinum <morten.teinum@gmail.com>
2016-03-13 11:16:46 +0000

    Converted framebuffer to a record, so we can keep some context data (rotation)

--- shfb.erl
+++ shfb.erl
@@ -4,6 +4,8 @@
 -module(shfb).
 -export([create/1, set_pixel/4, to_binary/1, set_data/2]).
 
+-record(framebuffer, {rotation, data}).
+
 %% setnth from http://stackoverflow.com/a/4781219/1167976
 setnth(1, [_|Rest], New) -> [New|Rest];
 setnth(I, [E|Rest], New) -> [E|setnth(I-1, Rest, New)].
@@ -12,20 +14,23 @@ create_row(Color) ->
 	[Color || _ <- lists:seq(1, 8)].
 
 create(Color) ->
-	[create_row(Color) || _ <- lists:seq(1, 8)].
+	#framebuffer {
+		rotation=0,
+		data=[create_row(Color) || _ <- lists:seq(1, 8)]
+	}.
 
 set_pixel(X, Y, Color, FB) ->
 	%% extract the row
-	RowToChange = lists:nth(X, FB),
+	RowToChange = lists:nth(X, FB#framebuffer.data),
 
 	%% change the column in that row
 	ChangedRow = setnth(Y, RowToChange, Color),
 
 	%% create a new matrix with the changed row
-	setnth(X, FB, ChangedRow).
+	FB#framebuffer { data=setnth(X, FB#framebuffer.data, ChangedRow) }.
 
 to_binary(FB) ->
-	list_to_binary([<<RGB:24>> || RGB <- lists:flatten(FB)]).
+	list_to_binary([<<RGB:24>> || RGB <- lists:flatten(FB#framebuffer.data)]).
 
-set_data(List, _) ->
-	List.
+set_data(List, FB) ->
+	FB#framebuffer { data = List }.
