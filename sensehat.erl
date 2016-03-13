59a84ee00b9cfe61eea90cfe4d2fcfb63f9c4639
Morten Teinum <morten.teinum@gmail.com>
2016-03-13 10:52:39 +0000

    Extract logo/0 into shexample

--- sensehat.erl
+++ sensehat.erl
@@ -7,7 +7,6 @@
 -export([set_pixel/3,
 	     clear/0,
 	     fill/1,
-	     logo/0, 
 	     fill_fb/1,
 	     get_gamma/0,
 	     set_gamma/1,
@@ -129,13 +128,4 @@ fill_fb(Data) when length(Data) =:= 8 ->
 clear() ->
 	fill(16#000000).
 
-logo() -> fill_fb(
-	[[16#ffffff, 16#ffffff, 16#ffffff, 16#ffffff, 16#ffffff, 16#ffffff, 16#ffffff, 16#ffffff],
-	 [16#fcf2f5, 16#ffffff, 16#fdf9fa, 16#eabec4, 16#f9e9ee, 16#ffffff, 16#fbeff2, 16#e9b1c1],
-	 [16#fdf7f9, 16#ffffff, 16#e192a7, 16#a70712, 16#da7a94, 16#ffffff, 16#fdf7f8, 16#cc4b6e],
-	 [16#ffffff, 16#ffffff, 16#fef9fa, 16#ffffff, 16#ffffff, 16#ffffff, 16#ffffff, 16#e39eb1],
-	 [16#ffffff, 16#ffffff, 16#da7d97, 16#ca5b69, 16#d1577d, 16#c94268, 16#bc2342, 16#c93f65],
-	 [16#ffffff, 16#ffffff, 16#cc4a6e, 16#930000, 16#9e0000, 16#a60005, 16#bc2444, 16#c73b61],
-	 [16#f8e6eb, 16#ffffff, 16#fbeff2, 16#c43658, 16#b31d2a, 16#e6a6b8, 16#ffffff, 16#d56886],
-     [16#f8e6ea, 16#ffffff, 16#ffffff, 16#ffffff, 16#fffdff, 16#ffffff, 16#fefbfc, 16#f0c9d4]]).
 
